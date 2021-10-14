31f1d57cb3ae225de06ea2763fed0d6b87bfdbd9
shuangshu <1103725164@qq.com>
2021-10-15 00:10:49 +0800

    加入check()，添加update()

--- Project_1/SqlTool.py
+++ Project_1/SqlTool.py
@@ -4,6 +4,9 @@ class SQLTool:
     #域
     con=None
     cursor=None
+    attributeName=None
+    tableName=None
+    #attributesName为表中属性名
     #方法
     def __init__(self):
         return
@@ -20,47 +23,67 @@ class SQLTool:
         )
         self.cursor=self.con.cursor()
 
-    def getAttributes(self, birdName, tableName):
-        #根据birdName从数据库中查询鸟类属性组成的元组
+    def check(self):
         if self.con==None:
             print("未连接数据库")
+            return False
+        if self.tableName==None:
+            print("未设置操作表名")
+            return False
+        return True
+
+    def setTable(self, tableName):
+        #设置表，同时获取表的属性名
+        self.tableName=tableName
+        if self.con==None:
+            print('未连接数据库')
             return
-        instruction='SELECT * FROM '+tableName+' WHERE 鸟名=\''+birdName+'\';'
+        self.cursor.execute('desc testBird')
+        DbDesc=self.cursor.fetchall()
+        temp=[]
+        for ele in DbDesc:
+            temp.append(ele[0])
+        self.attributeName=tuple(temp)
+
+    def getAttributes(self, birdName):
+        #根据birdName从数据库中查询鸟类属性组成的元组
+        if(not self.check()):
+            return
+        instruction='SELECT * FROM '+self.tableName+' WHERE 鸟名=\''+birdName+'\';'
         self.cursor.execute(instruction)
         result=self.cursor.fetchall()
         return result
 
-    def create(self, attributeNames, tableName):
-        #根据attributeNames新建一个关系列表，attributeNames为tuple对象
-        if self.con==None:
-            print("未连接数据库")
+    '''def create(self, attributeNames, attributesTypes):
+        #根据attributeNames新建一个关系列表，attributeNames为tuple对象，attributesTypes为数据类型的元组，其中元素为str类型
+        if(not self.check()):
             return
-        self.cursor.execute('CREATE TABLE '+tableName+'();')
-        return
+        self.cursor.execute('CREATE TABLE '+self.tableName+'();')
+        return'''
 
-    def update(self, tableName, attributes):
+    def update(self, attributes):
         #更新birdName的关系，attributes为tuple对象
-        if self.con==None:
-            print("未连接数据库")
+        if(not self.check()):
             return
-        
+        for i in range(len(self.attributeName)):
+            instruction="UPDATE "+self.tableName+' '+self.attributeName[i]+'='+attributes[i]+';'
+            self.cursor.execute(instruction)
         return
 
-    def insert(self, tableName, attributes):
+    def insert(self, attributes):
         #将attributes中的属性插入tableName对应的table
-        if self.con==None:
-            print("未连接数据库")
-            return
-        instruction='INSERT INTO '+tableName+' VALUES'+str(attributes)+';'
-        #print(instruction)
+        self.check()
+        instruction='INSERT INTO '+self.tableName+' VALUES'+str(attributes)+';'
         self.cursor.execute(instruction)
         self.con.commit()
         return
     
     def delete(self, birdName):
         #删除数据库中birdName的关系
-        if self.con==None:
-            print("未连接数据库")
+        if(not self.check()):
             return
-        
+        instruction="DELETE FROM "+self.tableName+' WHERE 鸟名='+'\''+birdName+'\';'
+        print(instruction)
+        self.cursor.execute(instruction)
+        self.con.commit()
         return
