831be203f7e92b2707d93c9ed8e2263925d41d00
shuangshu <1103725164@qq.com>
2021-10-15 15:36:11 +0800

    完成xlsTool测试

--- Project_1/XlsTool.py
+++ Project_1/XlsTool.py
@@ -1,19 +1,19 @@
-import xlwt as xw
+import xlrd as rd
 
 class XLSTool:
     #域
     workBook=None
+    filePath=None
     #方法
     #XLSTool用于打开.xls文件，并读取其中数据
-    def __init__(self):
-        return
-
     def __init__(self, filePath):
-        self.open_workbook(filePath)
+        self.workBook=rd.open_workbook(filePath)
+        self.filePath=filePath
     
     def openXls(self, filePath):
         #打开.xls（表格）文件
-        self.open_workbook(filePath)
+        self.workBook=rd.open_workbook(filePath)
+        self.filePath=filePath
     
     def readLines(self, sheetNum=0, start=0, end=-1):
         #读取xls文件中sheetNum的[start, end]行，返回一个列表，列表中元素为属性元组
@@ -23,8 +23,8 @@ class XLSTool:
             return
         sheet=self.workBook.sheets()[sheetNum]
         if end==-1:
-            end=sheet.rowNum
+            end=sheet.nrows
         result=[]
-        for i in range(start, end+1):
+        for i in range(start, end):
             result.append(tuple(sheet.row_values(i)))
         return result
